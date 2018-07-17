bd5ab373c3d39fef2ed9d17e21de5b524b09da50
Christos Dimizas <christos.dimizas@marinetraffic.com>
2018-07-17 15:57:32 +0300

    - Create Routes file for multiple files, - Import Routes file to client.js file which handles the client side react process, - Include Routes file also to SSR renderer process by passing the path from the express request. - And, enable express to watch all kinds of urls.

--- server/src/index.js
+++ server/src/index.js
@@ -10,10 +10,10 @@ const app = express();
 // Tell express to make the public folder available. With the static statement the bundle.js in the html below is
 // automatically pointed to the public folder.
 app.use(express.static('public'));
-// Express here watches the root url on port 3000 and responds with the <Home/> as a string
-app.get('/', (req, res) => {
+// Express here watches all urls on port 3000 and responds with the <Home/> as a string
+app.get('*', (req, res) => {
 
-    res.send(renderer());
+    res.send(renderer(req));
 });
 
 app.listen(3000, () => {
